7510007b2048a6311c0d1eafa2e3a36101cd4849
Oscar Pimentel <oscarpimentel@ua.pt>
2020-02-27 18:02:45 +0000

    Regioes Criticas e Classes Basicas (prototipo)

---
+++ BusDriver.java
@@ -0,0 +1,25 @@
+public class BusDriver{
+
+    int state;
+
+
+    public BusDriver(int s){
+        state = s;
+    }
+
+
+    public void setState(int s){
+        state = s;        
+    }
+
+    public int getState(){
+        return state;
+    }
+
+
+
+}
+
+
+
+
---
+++ Porter.java
@@ -0,0 +1,20 @@
+public class Porter{
+    
+    int state;
+
+    public Porter(int s){
+        state = s;
+    }
+
+
+    public void setState(int s){
+        state = s;        
+    }
+
+    public int getState(){
+        return state;
+    }
+
+
+
+}
